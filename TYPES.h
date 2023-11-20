addf399e94c7821d731823b9f1760885454202c3
AhmedAbdElmotelbAli <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2023-11-20 08:58:21 +0200

    Add files via upload

---
+++ TYPES.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H_INCLUDED
+#define TYPES_H_INCLUDED
+#include <stdint.h>
+typedef uint64_t i64_t;
+typedef uint32_t i32_t;
+typedef uint16_t i16_t;
+typedef uint8_t i8_t;
+typedef unsigned char i4_t;
+
+
+#endif // TYPES_H_INCLUDED
